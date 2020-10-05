72debed78edf0eadb97ce88d77b669e39e06518c
Ayman Abi Abdallah <aabiabdallah@netvariant.com>
2020-10-05 21:18:04 +0300

    Moved party services from scripts to src.

---
+++ src/main/groovy/com/mkdecision/dashboard/PartyServices.groovy
@@ -0,0 +1,129 @@
+package com.mkdecision.dashboard
+
+import org.moqui.context.*
+import org.moqui.entity.*;
+import org.moqui.util.*
+import org.apache.commons.lang3.StringUtils;
+
+class PartyServices {
+
+    static Map<String, Object> getPartyName(ExecutionContext ec) {
+
+        // shortcuts for convenience
+        ContextStack cs = ec.getContext()
+        EntityFacade ef = ec.getEntity()
+
+        // get the parameters
+        String partyId = (String) cs.getOrDefault("partyId", null)
+
+        // get the party
+        EntityValue partyDetail = ef.find("mantle.party.PartyDetail")
+                .condition("partyId", partyId)
+                .one();
+
+        // prepare party name
+        String partyName = null;
+        if (partyDetail != null) {
+            if (partyDetail.getString("partyTypeEnumId").equals("PtyPerson")) {
+                partyName = String.format("%s %s", StringUtils.defaultString(partyDetail.getString("firstName")), StringUtils.defaultString(partyDetail.getString("lastName"))).trim();
+            } else if (partyDetail.getString("partyTypeEnumId").equals("PtyOrganization")) {
+                partyName = partyDetail.getString("organizationName")
+            }
+        }
+
+        // return the output parameters
+        HashMap<String, Object> outParams = new HashMap<>()
+        outParams.put("partyName", partyName)
+        return outParams
+    }
+
+    static Map<String, Object> getPartyPrimaryEmailAddress(ExecutionContext ec) {
+
+        // shortcuts for convenience
+        ContextStack cs = ec.getContext()
+        EntityFacade ef = ec.getEntity()
+        UserFacade uf = ec.getUser()
+
+        // get the parameters
+        String partyId = (String) cs.getOrDefault("partyId", null)
+
+        // get the telecom numbers
+        EntityList emails = ef.find("mantle.party.contact.PartyContactMechInfo")
+                .condition("partyId", partyId)
+                .condition("contactMechPurposeId", "EmailPrimary")
+                .conditionDate("fromDate", "thruDate", uf.getNowTimestamp())
+                .list();
+
+        // prepare email address
+        String emailAddress = null;
+        if (emails != null && !emails.isEmpty()) {
+            EntityValue firstEmail = emails.getFirst();
+            emailAddress = firstEmail.getString("infoString");
+        }
+
+        // return the output parameters
+        HashMap<String, Object> outParams = new HashMap<>()
+        outParams.put("emailAddress", emailAddress)
+        return outParams
+    }
+
+    static Map<String, Object> getPartyPrimaryPostalAddress(ExecutionContext ec) {
+
+        // shortcuts for convenience
+        ContextStack cs = ec.getContext()
+        EntityFacade ef = ec.getEntity()
+        UserFacade uf = ec.getUser()
+
+        // get the parameters
+        String partyId = (String) cs.getOrDefault("partyId", null)
+
+        // get the postal addresses
+        EntityList postalAddresses = ef.find("mantle.party.contact.PartyContactMechPostalAddress")
+                .condition("partyId", partyId)
+                .condition("contactMechPurposeId", "PostalPrimary")
+                .conditionDate("fromDate", "thruDate", uf.getNowTimestamp())
+                .list();
+
+        // prepare address
+        String postalAddress = null;
+        if (postalAddresses != null && !postalAddresses.isEmpty()) {
+            EntityValue firstPostalAddress = postalAddresses.getFirst();
+            postalAddress = String.format("%s, %s %s", firstPostalAddress.getString("address1"), firstPostalAddress.getString("stateGeoCodeAlpha2"), firstPostalAddress.getString("postalCode"));
+        }
+
+        // return the output parameters
+        HashMap<String, Object> outParams = new HashMap<>()
+        outParams.put("postalAddress", postalAddress)
+        return outParams
+    }
+
+    static Map<String, Object> getPartyPrimaryTelecomNumber(ExecutionContext ec) {
+
+        // shortcuts for convenience
+        ContextStack cs = ec.getContext()
+        EntityFacade ef = ec.getEntity()
+        UserFacade uf = ec.getUser()
+
+        // get the parameters
+        String partyId = (String) cs.getOrDefault("partyId", null)
+
+        // get the telecom numbers
+        EntityList telecomNumbers = ef.find("mantle.party.contact.PartyContactMechTelecomNumber")
+                .condition("partyId", partyId)
+                .condition("contactMechPurposeId", "PhonePrimary")
+                .conditionDate("fromDate", "thruDate", uf.getNowTimestamp())
+                .list();
+
+        // prepare telecom number
+        String telecomNumber = null;
+        if (telecomNumbers != null && !telecomNumbers.isEmpty()) {
+            EntityValue firstNumber = telecomNumbers.getFirst();
+            telecomNumber = firstNumber.getString("contactNumber");
+        }
+
+        // return the output parameters
+        HashMap<String, Object> outParams = new HashMap<>()
+        outParams.put("telecomNumber", telecomNumber)
+        return outParams
+    }
+}
